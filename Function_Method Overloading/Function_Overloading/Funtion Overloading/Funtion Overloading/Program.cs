ae7e63249ff94eb950d3a32a8aeebc36eeb49e25
MohanSonia <31935268+MohanSonia@users.noreply.github.com>
2018-03-30 23:28:29 +0530

    Method Overloading

---
+++ Function_Method Overloading/Function_Overloading/Funtion Overloading/Funtion Overloading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Funtion_Overloading
+{
+    class Program
+    {
+        public string AddString(string F_Name, string L_Name)
+        {
+            return (F_Name + L_Name);
+        }
+
+        public string AddString(string F_Name, string M_Name, string L_Name)
+        {
+            return (F_Name + M_Name + L_Name);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Console.WriteLine("FullName:{0}", obj.AddString("Manoj", " Kandpal"));
+            Console.WriteLine("FullName:{0}", obj.AddString("Govind"," Ballabh"," Kandpal"));
+            Console.Read();
+
+        }
+    }
+}
---
+++ Function_Method Overloading/Method_Overloading/Method Overloading/Method Overloading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        public int Sum(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+        public int Sum(int a, int b, int c)
+        {
+            int result = a + b + c;
+            return result;
+        }
+        private float Sum(float a, float b)
+        {
+            float res = a + b;
+            return res;
+        }
+        private float Sum(float a, float b, float c)
+        {
+            float res = a + b + c;
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Console.WriteLine("The sum of Two Integers are:{0}", obj.Sum(12, 14));
+            Console.WriteLine("The sum of Three Integers are:{0}",obj.Sum(15, 25, 27));
+            Console.WriteLine("The sum of Two Floats are:{0}",obj.Sum(4.5f, 6.5f));
+            Console.WriteLine("The sum of Three Floats are:{0}",obj.Sum(5.9f, 6.7f, 8.5f));
+            Console.Read();
+        }
+    }
+}
